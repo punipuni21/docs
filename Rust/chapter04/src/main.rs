68ce8bd656461e3f1a1ecd0542517664631bbb66
punipuni21 <watanaberyotaroyp@gmail.com>
2024-01-21 22:08:46 +0900

    methods

--- Rust/chapter04/src/main.rs
+++ Rust/chapter04/src/main.rs
@@ -3,5 +3,6 @@ mod tuple_type;
 fn main() {
     // array_type::multidimensional();
     // tuple_type::dealare();
-    tuple_type::calc((10, 20));
+    // tuple_type::calc((10, 20));
+    tuple_type::methods();
 }
