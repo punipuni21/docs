d0fd895523510e11117a524dac5966fe8f46ccec
punipuni21 <watanaberyotaroyp@gmail.com>
2024-01-21 22:09:33 +0900

    methods

--- Rust/chapter04/src/tuple_type.rs
+++ Rust/chapter04/src/tuple_type.rs
@@ -16,3 +16,11 @@ pub fn calc(value: (i32, i32)) {
     println!("{} + {} = {}", value.0, value.1, value.0 + value.1);
     println!("{} - {} = {}", value.0, value.1, value.0 - value.1);
 }
+
+#[allow(dead_code)]
+pub fn methods() {
+    let a: (i32, i32, i32) = (1, 2, 3);
+    println!("clone():{:?}", a.clone());
+    println!("eq():{}", a.eq(&(1, 2, 3)));
+    println!("cmp():{:?}", a.cmp(&(2, 2, 3)));
+}
