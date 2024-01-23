aec124184f4d3dc79f315aa51bdd05b437d54f49
punipuni21 <watanaberyotaroyp@gmail.com>
2024-01-23 22:07:43 +0900

    pattern match

---
+++ Rust/chapter05/src/branch_match.rs
@@ -0,0 +1,24 @@
+#[allow(dead_code)]
+pub fn branch_1() {
+    let x = 10;
+    match x {
+        1 => println!("x is 1"),
+        2 => println!("x is 2"),
+        _ => println!("value is invalid"),
+    }
+
+    match x {
+        1 => {
+            let y = 100;
+            println!("y={}", y);
+        }
+        2 => {
+            let y = 200;
+            println!("y={}", y);
+        }
+        _ => {
+            let y = 300;
+            println!("y={}", y);
+        }
+    }
+}
