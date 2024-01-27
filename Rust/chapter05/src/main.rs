820741d43a05a8ace0b11d9cb1b324bfeb471324
punipuni21 <watanaberyotaroyp@gmail.com>
2024-01-28 00:34:28 +0900

    range

--- Rust/chapter05/src/main.rs
+++ Rust/chapter05/src/main.rs
@@ -4,5 +4,6 @@ fn main() {
     // branch_if::branch_if_let();
     // branch_match::branch_1();
     // branch_match::branch_2();
-    branch_match::branch_3();
+    // branch_match::branch_3();
+    branch_match::branch_4();
 }
