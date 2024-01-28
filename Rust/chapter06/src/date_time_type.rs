b0a80640d0250b3900a13269b8d1d636c1296656
punipuni21 <watanaberyotaroyp@gmail.com>
2024-01-28 22:16:16 +0900

    format

--- Rust/chapter06/src/date_time_type.rs
+++ Rust/chapter06/src/date_time_type.rs
@@ -6,3 +6,13 @@ pub fn instantiate() {
     let now: DateTime<Local> = Local::now();
     println!("Local datetime: {}", &now);
 }
+
+#[allow(dead_code)]
+pub fn format() {
+    let now: DateTime<Utc> = Utc::now();
+    let format_date = now.format("%Y年%m月%d日").to_string();
+    println!("{:?}", &format_date);
+    let now: DateTime<Local> = Local::now();
+    let format_date = now.format("%Y年%m月%d日 %H時%M分:%S秒").to_string();
+    println!("{:?}", &format_date);
+}
--- Rust/chapter06/src/main.rs
+++ Rust/chapter06/src/main.rs
@@ -3,5 +3,6 @@ mod string_type;
 fn main() {
     // string_type::instantiate();
     // string_type::add();
-    date_time_type::instantiate();
+    // date_time_type::instantiate();
+    date_time_type::format();
 }
