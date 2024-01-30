ead2c756464bc59712158535ac7cc9996cfc6ea0
punipuni21 <watanaberyotaroyp@gmail.com>
2024-01-30 21:59:53 +0900

    box

--- Rust/chapter06/src/main.rs
+++ Rust/chapter06/src/main.rs
@@ -1,8 +1,10 @@
+mod box_type;
 mod date_time_type;
 mod string_type;
 fn main() {
     // string_type::instantiate();
     // string_type::add();
     // date_time_type::instantiate();
-    date_time_type::format();
+    // date_time_type::format();
+    box_type::instantiate();
 }
