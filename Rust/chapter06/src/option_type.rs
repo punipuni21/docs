e114927fc7f133366f6d012136c8f0f7a8cad4a0
punipuni21 <watanaberyotaroyp@gmail.com>
2024-01-31 22:04:38 +0900

    convert option to result

--- Rust/chapter06/src/option_type.rs
+++ Rust/chapter06/src/option_type.rs
@@ -17,3 +17,21 @@ pub fn use_div() {
     };
     println!("{}", r);
 }
+
+#[allow(dead_code)]
+pub fn method_4() {
+    let x = 10;
+    let y = 5;
+    let r = match div(x, y).ok_or("cannot divide".to_owned()) {
+        Ok(result) => format!("{} / {} = {}", x, y, result),
+        Err(err) => err,
+    };
+    println!("ok_or: {}", r);
+    let x = 10;
+    let y = 0;
+    let r = match div(x, y).ok_or_else(|| "cannot divide".to_owned()) {
+        Ok(result) => format!("{} / {} = {}", x, y, result),
+        Err(err) => err,
+    };
+    println!("ok_or_else: {}", r);
+}
