01669dae097e379ef06decdebf93a8f3951a9ec0
punipuni21 <watanaberyotaroyp@gmail.com>
2024-02-06 22:28:47 +0900

    csv desirialize

--- Rust/chapter10/src/generics::traits_impl.rs
+++ Rust/chapter10/src/generics::traits_impl.rs
@@ -1,4 +1,6 @@
+use csv::ReaderBuilder;
 use std::marker::PhantomData;
+use std::path::PathBuf;
 
 #[derive(Defualt)]
 pub struct CsvReaderImpl<T> {
@@ -9,7 +11,17 @@ where
     T: DeserializeOwned,
 {
     fn read(&self, file_path: &str) -> Result<Vec<T>> {
-        todo!()
+        let path_buf = PathBuf::from(file_path);
+        let string_data = read_to_string(path_buf);
+        let mut reader = ReaderBuilder::new()
+            .delimiter(b',')
+            .from_reader(string_data.as_bytes());
+        let rows = reader.desirialize::<T>();
+        let mut result = Vec::<T>::new();
+        for row in rows {
+            result.push(row?);
+        }
+        Ok(result)
     }
 }
 
