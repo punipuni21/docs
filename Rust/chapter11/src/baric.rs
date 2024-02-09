1d3a144d289f8262cef3ec9d9b80fbf4125a2ea8
punipuni21 <watanaberyotaroyp@gmail.com>
2024-02-09 22:49:07 +0900

    kind

--- Rust/chapter11/src/baric.rs
+++ Rust/chapter11/src/baric.rs
@@ -1,3 +1,6 @@
+use std::env::current_dir;
+use std::fs::File;
+use std::io::ErrorKind;
 #[derive(Debug, PartialEq, Eq)]
 pub enum ValueConversion {
     Int,
@@ -19,4 +22,21 @@ pub fn use_parse_01() {
     parse_01(String::from("123"), ValueConversion::Float);
     parse_01(String::from("ABC"), ValueConversion::Int);
     parse_01(String::from("ABC"), ValueConversion::Float);
+}
+
+#[allow(dead_code)]
+pub fn use_error_kind() {
+    let file_path = current_dir()
+        .map(|path_buf| path_buf.join("\\files\\sample.txt"))
+        .map_err(|error| panic!("{}", error))
+        .unwrap();
+
+    let error = File::open(file_path).err().unwrap();
+
+    let result = match error.kind() {
+        ErrorKind::NotFound => "File not found",
+        ErrorKind::PermissionDenied => "Permission denied",
+        _ => "Other error",
+    };
+    println!("{}", result);
 }
--- Rust/chapter11/src/main.rs
+++ Rust/chapter11/src/main.rs
@@ -1,4 +1,5 @@
 mod baric;
 fn main() {
-    baric::use_parse_01();
+    // baric::use_parse_01();
+    baric::use_error_kind();
 }
