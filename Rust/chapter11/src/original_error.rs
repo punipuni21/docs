4365caf448b7924d9ee0646283fdf770a0496bf2
punipuni21 <watanaberyotaroyp@gmail.com>
2024-02-10 23:48:08 +0900

    original error impl

--- Rust/chapter11/src/original_error.rs
+++ Rust/chapter11/src/original_error.rs
@@ -1,7 +1,25 @@
 use num_trates::NumOps;
+use std::error::Error;
+use std::fmt::{Display, Formatter};
+use std::num::{ParseFloatError, ParseIntError};
 use std::str::FromStr;
 
 #[allow(dead_code)]
 pub fn parse_02<T: NumOps + FromStr>(value: String) -> Result<T, E> {
     todo!()
+}
+
+#[derive(Debug)]
+pub enum SampleError {
+    IntError(ParseIntError),
+    FloatError(ParseFloatError),
+}
+impl Error for SampleError {}
+impl Display for SampleError {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        match self {
+            SampleError::IntError(err) => write!(f, "IntError: {}", err),
+            SampleError::FloatError(err) => write!(f, "FloatError: {}", err),
+        }
+    }
 }
