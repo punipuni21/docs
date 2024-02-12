8d0aa40d5c8f3e02b58f8434c6e151b02edae3e1
punipuni21 <watanaberyotaroyp@gmail.com>
2024-02-12 22:17:41 +0900

    from attribute

--- Rust/chapter11/src/main.rs
+++ Rust/chapter11/src/main.rs
@@ -1,7 +1,9 @@
 mod baric;
 mod original_error;
+mod this_error;
 fn main() {
     // baric::use_parse_01();
     // baric::use_error_kind();
-    original_error::use_parse_02();
+    // original_error::use_parse_02();
+    this_error::use_parse_03();
 }
--- Rust/chapter11/src/this_error.rs
+++ Rust/chapter11/src/this_error.rs
@@ -1,19 +1,47 @@
+use num_traits::NumOps;
+use std::num::{ParseFloatError, ParseIntError};
+use std::str::FromStr;
+use thiserror::Error;
+
 #[derive(Debug, Error)]
 pub enum SampleError {
-    #[error("IntError: {0}")]
-    IntError(String),
-    #[error("FloatError: {0}")]
-    FloatError(String),
+    #[error(transparent)]
+    IntError(#[from] ParseIntError),
+    #[error(transparent)]
+    FloatError(#[from] ParseFloatError),
 }
 
-impl From<ParseIntError> for SampleError {
-    fn from(value: String) -> Self {
-        Self::IntError(value.to_string())
-    }
+#[allow(dead_code)]
+pub fn parse_03<T: NumOps + FromStr>(value: String) -> Result<T, SampleError>
+where
+    SampleError: From<<T as FromStr>::Err>,
+{
+    let result = value
+        .parse::<T>()
+        .map_err(|error| SampleError::from(error))?;
+    Ok(result)
 }
 
-impl From<ParseFloatError> for SampleError {
-    fn from(value: String) -> Self {
-        Self::FloatError(value.to_string())
-    }
+// impl From<ParseIntError> for SampleError {
+//     fn from(value: ParseIntError) -> Self {
+//         Self::IntError(value.to_string())
+//     }
+// }
+
+// impl From<ParseFloatError> for SampleError {
+//     fn from(value: ParseFloatError) -> Self {
+//         Self::FloatError(value.to_string())
+//     }
+// }
+
+#[allow(dead_code)]
+pub fn use_parse_03() {
+    let result = parse_03::<i32>(String::from("123")).unwrap();
+    println!("{:?}", result);
+    let result = parse_03::<i32>(String::from("ABC")).err().unwrap();
+    println!("{:?}", result.to_string());
+    let result = parse_03::<f32>(String::from("123")).unwrap();
+    println!("{:?}", result);
+    let result = parse_03::<f32>(String::from("ABC")).err().unwrap();
+    println!("{:?}", result.to_string());
 }
