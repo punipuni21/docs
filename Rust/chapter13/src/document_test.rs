3ab95382e4d78ed7e3fede3df03023916a3adcdb
punipuni21 <watanaberyotaroyp@gmail.com>
2024-02-18 22:49:18 +0900

    allow dead code

--- Rust/chapter13/src/document_test.rs
+++ Rust/chapter13/src/document_test.rs
@@ -6,6 +6,7 @@ use crate::target::Guest;
 /// assert_eq!(500 , result);
 /// ```
 
+#[allow(dead_code)]
 pub fn calc_fee_case_01() -> u32 {
     let guest = Guest::new(10, false);
     let result = guest.clone().calc_fee().unwrap();
