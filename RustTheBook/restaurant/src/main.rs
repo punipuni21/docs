42f3b7cef2a6280671702c00d04bb3d4d7a92c68
punipuni21 <watanaberyotaroyp@gmail.com>
2024-02-25 22:08:55 +0900

    other crate

--- RustTheBook/restaurant/src/main.rs
+++ RustTheBook/restaurant/src/main.rs
@@ -1,4 +1,8 @@
 use restaurant::eat_at_restaurant;
+use std::collections::HashMap; //他のクレートの公開されているものを使う場合はフルパスで記載する
 fn main() {
     eat_at_restaurant();
+
+    let mut map = HashMap::new();
+    map.insert(1, 2);
 }
