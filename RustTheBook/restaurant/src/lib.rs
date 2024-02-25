df0bc6161964ef06209a3a29069f67455e6e4eb1
punipuni21 <watanaberyotaroyp@gmail.com>
2024-02-25 21:56:18 +0900

    struct enum

--- RustTheBook/restaurant/src/lib.rs
+++ RustTheBook/restaurant/src/lib.rs
@@ -15,6 +15,27 @@ mod front_of_house {
 }
 
 mod back_of_house {
+
+    pub struct Breakfast {
+        //構造体は公開，フィールドは非公開
+        pub toast: String,
+        seasonal_fruit: String,
+    }
+
+    impl Breakfast {
+        pub fn summer(toast: &str) -> Breakfast {
+            Breakfast {
+                toast: String::from(toast),
+                seasonal_fruit: String::from("peaches"),
+            }
+        }
+    }
+
+    pub enum Appetizer {
+        Soup,
+        Salad,
+    }
+
     fn fix_incorrect_order() {
         cook_order();
         super::front_of_house::serving::serve_order();
@@ -24,9 +45,11 @@ mod back_of_house {
 }
 
 pub fn eat_at_restaurant() {
-    // Absolute path
-    crate::front_of_house::hosting::add_to_waitlist();
+    let mut meal = back_of_house::Breakfast::summer("Rye");
 
-    // Relative path
-    front_of_house::hosting::add_to_waitlist();
+    meal.toast = String::from("Wheat");
+    println!("I'd like {} toast please", meal.toast);
+
+    let order1 = back_of_house::Appetizer::Soup;
+    let order2 = back_of_house::Appetizer::Salad;
 }
