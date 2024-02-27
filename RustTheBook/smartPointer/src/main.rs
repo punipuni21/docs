295736901f2092456ec6f30bedb506d3a875a5db
punipuni21 <watanaberyotaroyp@gmail.com>
2024-02-27 22:45:25 +0900

    参照外し

--- RustTheBook/smartPointer/src/main.rs
+++ RustTheBook/smartPointer/src/main.rs
@@ -5,7 +5,14 @@ enum List {
 
 use List::{Cons, Nil};
 
+#[allow(unused_variables)]
 fn main() {
     let list = Cons(1, Box::new(Cons(2, Box::new(Cons(3, Box::new(Nil))))));
     //Boxは間接参照とヒープメモリ確保を提供する
+
+    let x = 5;
+    let y = Box::new(x);
+
+    assert_eq!(5, x);
+    assert_eq!(5, *y);
 }
